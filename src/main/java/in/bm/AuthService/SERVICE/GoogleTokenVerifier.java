b8e9aa62b67f0ce3a0627f6c7d68274927006de0
BharatMarwah <bharatmarwah4@gmail.com>
2025-12-30 18:44:56 +0530

    feat: add Google ID token verification

--- src/main/java/in/bm/AuthService/SERVICE/GoogleTokenVerifier.java
+++ src/main/java/in/bm/AuthService/SERVICE/GoogleTokenVerifier.java
@@ -1,6 +1,8 @@
 package in.bm.AuthService.SERVICE;
 
-import in.bm.AuthService.DTO.GoogleUserInfo;
+import in.bm.AuthService.EXCEPTION.OauthAuthenticationException;
+import in.bm.AuthService.RESPONSEDTO.GoogleUserInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -8,17 +10,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GoogleTokenVerifier {
-
     private final JwtDecoder jwtDecoder;
 
-    public GoogleTokenVerifier(){
-        this.jwtDecoder= JwtDecoders
+    private final String clientId;
+
+    public GoogleTokenVerifier(@Value("${client-id}") String clientId) {
+        this.jwtDecoder = JwtDecoders
                 .fromIssuerLocation
                         ("https://accounts.google.com");// Google public key for verification
+
+        this.clientId = clientId;
     }
 
-    public GoogleUserInfo verify(String idToken){
+    public GoogleUserInfo verify(String idToken) {
         Jwt jwt = jwtDecoder.decode(idToken);
+
+        if (!jwt.getAudience().contains(clientId)){
+            throw new OauthAuthenticationException("Invalid token audience");
+        }
+
+        Boolean emailVerified = jwt.getClaim("email_verified");
+        if (emailVerified == null || !emailVerified){
+            throw new OauthAuthenticationException("Email not verified by Google");
+        }
 
         return GoogleUserInfo
                 .builder()
@@ -27,7 +41,6 @@ public class GoogleTokenVerifier {
                 .subject(jwt.getSubject())
                 .build();
     }
-
 
 
 }
